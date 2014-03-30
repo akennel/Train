6e947ee2dd221c276bd57c57d730c60966d0f105
Andrew Kennel <akennel@gmail.com>
2014-03-29 20:33:44 -0400

    Made a stap at the right path
    Updated widget code for right names and descriptions.

--- GetTrainInfo.php
+++ GetTrainInfo.php
@@ -1,21 +1,21 @@
 <?php
-/* Plugin Name: Phila Pay Widget
-Plugin URI: localhost/wordpress
-Description: Displays Payment Options
+/* Plugin Name: Get Train Info
+Plugin URI: http://turtlemafia.org
+Description: Displays Train status for East Falls SEPTA Trains.
 Version: 1.0
 Author: Andrew Kennel
-Author URI: localhost/wordpress
+Author URI: http://turtlemafia.org
 */
-add_shortcode('PhilaPay', 'philapay_handler');
+add_shortcode('Train', 'train_handler');
 
-function philapay_handler(){
+function train_handler(){
     $message = <<<EOM
 	
 	<!DOCTYPE HTML>
     <head>
 
 		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-        <script src="GetTrainInfo.js"></script>
+        <script src="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/GetTrainInfo.js"></script>
 
         <link rel="stylesheet" href="style.css" />
         <title>East Falls Train Schedule</title>
@@ -44,10 +44,10 @@ EOM;
 return $message;
 }
 
-function philapaywidget($args, $instance) { // widget sidebar output
+function trainwidget($args, $instance) { // widget sidebar output
   extract($args, EXTR_SKIP);
   echo $before_widget; // pre-widget code from theme
-  echo philapay_handler();
+  echo train_handler();
   echo $after_widget; // post-widget code from theme
 }
 ?>
