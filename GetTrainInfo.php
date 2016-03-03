ec608115c19f46565f23f0da357bac2026017d26
akennel <akennel@gmail.com>
2016-03-03 17:08:37 -0500

    Updated links for turtlemafia loc

--- GetTrainInfo.php
+++ GetTrainInfo.php
@@ -15,29 +15,54 @@ function train_handler(){
     <head>
 
 		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+        <script src="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/cookies.js"></script>
         <script src="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/GetTrainInfo.js"></script>
 
         <link rel="stylesheet" href="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/style.css" />
         <title>East Falls Train Schedule</title>
         
+        <title>East Falls Train Schedule</title>
+            
         <script type="text/javascript">
-            $(document).ready(function () {
+          $(document).ready(function () {
 
-                GetSchedule();
+            StartApp();
 
-            });
-        	
+          });
+              
         </script>
-        
-    </head>
-    <body>
-        <h2 id="Inbound">East Falls Inbound</h2>
-        <ul id="Inbound-List">
-        </ul>
-        <h2 id="Outbound">Market East Outbound</h2>
-        <ul id="Outbound-List">
-        </ul>
-    </body>
+            
+      </head>
+      <body>
+        <div id = "Time" style="display:none;">
+          <h2 id="Inbound">East Falls Inbound</h2>
+          <ul id="Inbound-List">
+          </ul>
+          <h2 id="Outbound">Jefferson Outbound</h2>
+          <ul id="Outbound-List">
+          </ul>
+          <div>
+            <h3 id="Line"></h3>
+            <img src = "http://turtlemafia.org/wp-content/plugins/GetTrainInfo/gear.jpg" id = "GearImage" onclick="ShowSettings()"></img>
+          </div>
+        </div>
+        <div id = "Settings" style="display:none;">
+          <div>
+            <h2>Select your Line...</h2>
+            <select id="LinesList" onchange="GetStationsForLine()"></select>
+          </div>
+          <div>
+          <h2>Select your Home Station...</h2>
+            <select id="HomeStationList"></select>
+            <h2>Select your Work Station...</h2>
+            <select id="WorkStationList"></select>
+          </div>
+          <div>
+            <img id="SaveImage" onclick="SaveChanges()" src="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/save.jpg"></img>
+            <img id="CancelImage" onclick="CancelChanges()" src="http://turtlemafia.org/wp-content/plugins/GetTrainInfo/cancel.jpg"></img>
+          </div>
+        </div>
+      </body>
 	
 EOM;
 
