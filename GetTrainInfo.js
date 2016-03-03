6e3cb57a6626bdabdef927d3894e79a21470f0e8
akennel <akennel@gmail.com>
2016-03-03 16:19:53 -0500

    Added Settings
    Added Settings screen. Saved prefs as cookie.

--- GetTrainInfo.js
+++ GetTrainInfo.js
@@ -1,6 +1,96 @@
+function ShowSettings(){
+  $("#Time").fadeOut("fast");
+  $("#Settings").fadeIn("fast");
+}
+
+function ShowTimes(){
+  $("#Settings").fadeOut("fast");
+  $("#Time").fadeIn("fast");  
+}
+
+function SaveChanges(){
+  setCookie("HomeStationName", $("#HomeStationList :selected").text()); 
+  setCookie("WorkStationName", $("#WorkStationList :selected").text());
+  setCookie("LineName", $("#LinesList :selected").text());
+  setCookie("LineID", $("#LinesList :selected").val());
+  GetSchedule();
+  $("#Settings").fadeOut("fast");
+  $("#Time").fadeIn("fast");  
+}
+function CancelChanges(){
+  if (DoesCookieExist()){
+    $("#Settings").fadeOut("fast");
+    $("#Time").fadeIn("fast"); 
+  }  
+}
+
+function GetLines(){
+  var linesAPI = "lines.json";
+  $.getJSON(linesAPI, function (data){
+    $("#LinesList").empty();
+    var i = 0;
+    while(i<data.length){
+      $("#LinesList").append("<option value=" + data[i].Line_ID + ">" + data[i].Line_Name + "</option>");
+      i++;
+    }
+    
+    var selectedLine = getCookie("LineID");
+    if (selectedLine != null){
+      $("#LinesList").val(selectedLine);
+      GetStations(selectedLine);
+    }
+    else{
+      GetStations("AIR");
+    }
+  });
+}
+
+function DoesCookieExist(){
+  var homeStationName = getCookie("HomeStationName");
+  var workStationName = getCookie("WorkStationName");
+  
+  if (homeStationName != null && workStationName != null){
+    return true;
+  }
+  else{
+    return false;
+  }    
+}
+
+function GetStations(lineID){
+  var stationAPI = "stations.json";
+  $.getJSON(stationAPI, function (data){
+    $("#HomeStationList").empty();
+    $("#WorkStationList").empty();
+    var i = 0;
+    while(i<data.length){      
+      var currentStationLine = data[i].Station_Line;
+      var currentStationName = data[i].Station_Name;
+      if (currentStationLine.toString().indexOf(lineID) >= 0){
+        var currentStation = "<option value=" + data[i].Station_Name + ">" + data[i].Station_Name + "</option>";
+        if (currentStationName != "30th Street Station" && currentStationName != "Suburban Station" && currentStationName != "Market East" && currentStationName != "Temple U"){
+          $("#HomeStationList").append(currentStation);
+        }
+        if (currentStationName == "30th Street Station" || currentStationName == "Suburban Station" || currentStationName == "Market East" || currentStationName == "Temple U"){
+          $("#WorkStationList").append(currentStation);
+        }
+      }            
+      i++;
+    }
+  });
+}
+
+function GetStationsForLine(){
+  var selectedLine = $("#LinesList :selected").val();
+  GetStations(selectedLine);
+}
+
 function GetSchedule() {
-    var InboundAPI = "http://www3.septa.org/hackathon/NextToArrive/?req1=East%20Falls&req2=Market%20East&req3=2&callback=?";
-    var OutboundAPI = "http://www3.septa.org/hackathon/NextToArrive/?req1=Market%20East&req2=East%20Falls&req3=2&callback=?"
+    $("#Line").text(getCookie("LineName"));
+    $("#Inbound").text(getCookie("HomeStationName") + " Inbound");
+    $("#Outbound").text(getCookie("WorkStationName") + " Outbound");
+    var InboundAPI = "http://www3.septa.org/hackathon/NextToArrive/?req1=" + getCookie("HomeStationName") + "&req2=" + getCookie("WorkStationName") + "&req3=2&callback=?";
+    var OutboundAPI = "http://www3.septa.org/hackathon/NextToArrive/?req1=" + getCookie("WorkStationName")  + "&req2=" + getCookie("HomeStationName") + "&req3=2&callback=?"
     
     $.getJSON(InboundAPI, function (data) {
         $("#Inbound-List").empty();
@@ -58,4 +148,16 @@ function GetSchedule() {
 			i++;
         }
     });
+}
+
+function StartApp(){
+  if (DoesCookieExist()){
+    GetSchedule();
+    ShowTimes();
+    GetLines();
+  }
+  else{
+    GetLines();
+    ShowSettings();
+  }
 }
---
+++ cookies.js
@@ -0,0 +1,16 @@
+function setCookie(c_name, value) {
+    var c_value = value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    document.cookie = c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return (y);
+        }
+    }
+}
